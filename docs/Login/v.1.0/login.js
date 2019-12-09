55a30967be564f7f0be8ef195906995ddeccf475
FBROTHE534studentglendaleedu <58280240+FBROTHE534studentglendaleedu@users.noreply.github.com>
2019-12-09 14:09:06 +0700

    Update login.js

--- docs/Login/v.1.0/login.js
+++ docs/Login/v.1.0/login.js
@@ -3,5 +3,5 @@ function loginSubmit() {
 }
 $(document).ready(function(){
   $("#btnSubmit").click(function(){
-    alert('Đăng nhập thành công!');
+    alert('Đăng nhập thành công theo cách document ready!');
 });
